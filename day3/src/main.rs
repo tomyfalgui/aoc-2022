2e42883b34ba8ad81b476085b8bfb9a771892227
Tomy Falgui <tomyfalgui@gmail.com>
2022-12-07 00:24:03 +0800

    extract calculate priority function

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,7 +3,10 @@ use std::collections::HashSet;
 fn main() {
     let input = include_str!("../input.txt");
     part_one(input);
+    part_two(input);
 }
+
+fn part_two(input: &str) {}
 
 fn part_one(input: &str) {
     let mut total_priorities = 0;
@@ -22,16 +25,22 @@ fn part_one(input: &str) {
         // only one intersection
 
         if let Some(val) = intersection.next() {
-            let mut code_point = *val as u32;
-            // a - z
-            if code_point >= 97 {
-                code_point -= 96;
-            } else if code_point >= 65 {
-                code_point = code_point - 65 + 27;
-            }
+            let code_point = calculate_priority(val);
             total_priorities += code_point;
         }
     }
 
     println!("Total: {total_priorities}");
+}
+
+fn calculate_priority(char: &char) -> u32 {
+    let mut code_point = *char as u32;
+    // a - z
+    if code_point >= 97 {
+        code_point -= 96;
+    } else if code_point >= 65 {
+        code_point = code_point - 65 + 27;
+    }
+
+    code_point
 }
