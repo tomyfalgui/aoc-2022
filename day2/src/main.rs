7182e82ee98f2d21d9e496a8d6b35886c6da8e0a
Tomy Falgui <tomyfalgui@gmail.com>
2022-12-06 19:59:14 +0800

    day 2 part 1

---
+++ day2/src/main.rs
@@ -0,0 +1,70 @@
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+enum Choice {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl FromStr for Choice {
+    type Err = ();
+    fn from_str(input: &str) -> Result<Choice, Self::Err> {
+        match input {
+            "X" | "A" => Ok(Choice::Rock),
+            "Y" | "B" => Ok(Choice::Paper),
+            "Z" | "C" => Ok(Choice::Scissors),
+            _ => Err(()),
+        }
+    }
+}
+
+fn main() {
+    let input = include_str!("../input.txt");
+
+    let mut total_score = 0;
+    for line in input.lines() {
+        let split: Vec<&str> = line.trim_end().split(' ').collect();
+
+        let opponent = Choice::from_str(split[0]).unwrap();
+        let player = Choice::from_str(split[1]).unwrap();
+
+        let score = check_decision(&opponent, &player);
+        total_score += score;
+        match player {
+            Choice::Rock => total_score += 1,
+            Choice::Paper => total_score += 2,
+            Choice::Scissors => total_score += 3,
+        }
+    }
+
+    println!("{}", total_score);
+}
+
+fn check_decision(opponent: &Choice, player: &Choice) -> usize {
+    if let Choice::Rock = player {
+        if let Choice::Rock = opponent {
+            3
+        } else if let Choice::Paper = opponent {
+            0
+        } else {
+            6
+        }
+    } else if let Choice::Paper = player {
+        if let Choice::Rock = opponent {
+            6
+        } else if let Choice::Paper = opponent {
+            3
+        } else {
+            0
+        }
+    } else {
+        if let Choice::Rock = opponent {
+            0
+        } else if let Choice::Paper = opponent {
+            6
+        } else {
+            3
+        }
+    }
+}
