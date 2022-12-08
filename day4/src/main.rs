9c840569c519ed3f0947cdf7e9060c087fbc5a29
Tomy Falgui <tomyfalgui@gmail.com>
2022-12-08 16:22:18 +0800

    day 4 part - change all to any

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -3,6 +3,8 @@ fn main() {
 
     let answ = part_one(input);
     println!("{answ}");
+    let answ = part_two(input);
+    println!("{answ}");
 }
 
 fn part_one(input: &str) -> usize {
@@ -28,6 +30,39 @@ fn part_one(input: &str) -> usize {
                 .collect::<Vec<usize>>()
                 .iter()
                 .all(|z| a_copy.contains(&z));
+            if a_vec || b_vec {
+                Some(1)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>()
+        .len()
+}
+
+fn part_two(input: &str) -> usize {
+    input
+        .lines()
+        .filter_map(|line| {
+            let pair = line.split(',').collect::<Vec<&str>>();
+            let range_one = pair[0];
+            let range_two = pair[1];
+            let split = range_one
+                .split('-')
+                .map(|x| x.parse().unwrap())
+                .collect::<Vec<usize>>();
+            let split_two = range_two
+                .split('-')
+                .map(|x| x.parse().unwrap())
+                .collect::<Vec<usize>>();
+
+            let (a, b) = (split[0]..=split[1], split_two[0]..=split_two[1]);
+            let a_copy = a.clone();
+            let a_vec = a.collect::<Vec<usize>>().iter().any(|z| b.contains(&z));
+            let b_vec = b
+                .collect::<Vec<usize>>()
+                .iter()
+                .any(|z| a_copy.contains(&z));
             if a_vec || b_vec {
                 Some(1)
             } else {
