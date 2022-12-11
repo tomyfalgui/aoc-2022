54dbbcf19518fcd36d8da1697a95845a377e1f4f
Tomy Falgui <tomyfalgui@gmail.com>
2022-12-11 17:00:25 +0800

    finish part 1

---
+++ day8/src/main.rs
@@ -0,0 +1,93 @@
+fn main() {
+    let input = include_str!("../input.txt").trim();
+
+    let tree = parse_input(input);
+    let tree_length = tree.len();
+
+    let mut visible_trees = 0;
+    for (row, tree_line) in tree.iter().enumerate() {
+        if row == 0 || row == (tree_length - 1) {
+            visible_trees += tree_line.len();
+            continue;
+        }
+        let tree_line_length = tree_line.len();
+        for (column, _curr_tree) in tree_line.iter().enumerate() {
+            if column == 0 || column == (tree_line_length - 1) {
+                visible_trees += 1;
+                continue;
+            }
+            if get_all_top(&tree, row, column)
+                || get_all_down(&tree, row, column)
+                || get_all_left(&tree, row, column)
+                || get_all_right(&tree, row, column)
+            {
+                visible_trees += 1;
+            }
+        }
+    }
+
+    println!("There are {} visible trees", visible_trees);
+}
+fn get_all_down(tree_lines: &Vec<Vec<u64>>, tree_row: usize, tree_column: usize) -> bool {
+    let row_length = tree_lines.len();
+
+    let mut is_greater = true;
+    for x in (tree_row + 1)..row_length {
+        if tree_lines[x][tree_column] >= tree_lines[tree_row][tree_column] {
+            is_greater = false;
+            break;
+        }
+    }
+
+    is_greater
+}
+fn get_all_right(tree_lines: &Vec<Vec<u64>>, tree_row: usize, tree_column: usize) -> bool {
+    let column_length = tree_lines[0].len();
+
+    let mut is_greater = true;
+    for x in (tree_column + 1)..column_length {
+        if tree_lines[tree_row][x] >= tree_lines[tree_row][tree_column] {
+            is_greater = false;
+            break;
+        }
+    }
+
+    is_greater
+}
+fn get_all_left(tree_lines: &Vec<Vec<u64>>, tree_row: usize, tree_column: usize) -> bool {
+    let mut is_greater = true;
+    for x in (0..tree_column).rev() {
+        if tree_lines[tree_row][x] >= tree_lines[tree_row][tree_column] {
+            is_greater = false;
+            break;
+        }
+    }
+
+    is_greater
+}
+
+fn get_all_top(tree_lines: &Vec<Vec<u64>>, tree_row: usize, tree_column: usize) -> bool {
+    let mut is_greater = true;
+    for x in (0..tree_row).rev() {
+        if tree_lines[x][tree_column] >= tree_lines[tree_row][tree_column] {
+            is_greater = false;
+            break;
+        }
+    }
+
+    is_greater
+}
+
+fn parse_input(input: &str) -> Vec<Vec<u64>> {
+    let mut vec: Vec<Vec<u64>> = Vec::new();
+
+    for line in input.lines() {
+        let mut inner_vec: Vec<u64> = Vec::new();
+        line.chars()
+            .for_each(|x| inner_vec.push(x.to_digit(10).unwrap().into()));
+
+        vec.push(inner_vec);
+    }
+
+    vec
+}
