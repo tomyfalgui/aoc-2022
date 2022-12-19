53e03d64619058b32ea28e04d307b31899fde42e
Tomy Falgui <tomyfalgui@gmail.com>
2022-12-19 20:15:51 +0800

    day 12 init

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -2,11 +2,20 @@ fn main() {
     let input = include_str!("../test.txt").trim();
 
     let rows = parse_input(input);
-    let start_pos = find_letter(&rows, 'S');
-    let end_pos = find_letter(&rows, 'E');
+    let curr_pos = find_letter(&rows, 'S', &vec![]);
+    let end_pos = find_letter(&rows, 'E', &vec![]);
     let num_rows = convert_rows(rows);
 
-    println!("{:?}", num_rows);
+    loop {
+        let mut letters = 'a' as u64..'z' as u64;
+        let mut found_positions: Vec<(u64, u64)> = Vec::new();
+        for letter in letters {
+            println!("{}", letter);
+            let found_position = find_letter(&num_rows, letter, &found_positions);
+            println!("{:?}", found_position);
+        }
+        break;
+    }
 }
 
 fn parse_input(input: &str) -> Vec<Vec<char>> {
@@ -16,13 +25,20 @@ fn parse_input(input: &str) -> Vec<Vec<char>> {
         .collect()
 }
 
-fn find_letter(input: &Vec<Vec<char>>, letter: char, skip: (u64, u64)) -> (u64, u64) {
+fn find_letter<T: std::cmp::PartialEq>(
+    input: &Vec<Vec<T>>,
+    letter: T,
+    skip: &Vec<(u64, u64)>,
+) -> (u64, u64) {
     let mut pos = (0, 0);
     let input_len = input.len();
 
     'outer: for i in 0..input_len {
         let column_len = input[i].len();
         for j in 0..column_len {
+            if skip.contains(&(i as u64, j as u64)) {
+                continue;
+            }
             if letter == input[i][j] {
                 pos = (i as u64, j as u64);
                 break 'outer;
