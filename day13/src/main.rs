c16d9a358d510eabca5940b4eda2d584d510948c
Tomy Falgui <tomyfalgui@gmail.com>
2022-12-20 18:09:18 +0800

    day 13 progress

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,3 +1,19 @@
+enum Element {
+    Char(char),
+    Vec(Vec<char>),
+}
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../test.txt").trim();
+
+    for pair in input.split("\n\n") {
+        let pair_split = pair.split("\n").collect::<Vec<&str>>();
+
+        let first: Vec<Element> = Vec::new();
+        let first_item = pair_split[0].chars();
+        let mut count_brackets = 0;
+
+        for f_char in first_item {
+            println!("{:?}", f_char);
+        }
+    }
 }
